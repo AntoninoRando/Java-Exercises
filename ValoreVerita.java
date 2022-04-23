073160ff36e869a5dd5766f1a9b42a2bafef550b
AntoninoRando <antonino.rando.2003@gmail.com>
2022-04-24 01:40:27 +0200

    Migliorie all'enumeratore

--- ValoreVerita.java
+++ ValoreVerita.java
@@ -1,17 +1,23 @@
 package EspressioniDiVerita;
 
 public enum ValoreVerita {
-    VERO(true), FALSO(false);
+    VERO(true, 1), FALSO(false, 0);
 
     private boolean stato;
+    private int statoIntero;
 
     // Costruttore
-    ValoreVerita(boolean stato) {
+    ValoreVerita(boolean stato, int statoIntero) {
         this.stato = stato;
+        this.statoIntero = statoIntero;
     }
 
     public boolean toBoolean() {
         return stato;
+    }
+
+    public int toInt() {
+        return statoIntero;
     }
 
     public static ValoreVerita fromBoolean(Boolean stato) {
