0319455381694610df4699d12c6e8b16919dc38d
AntoninoRando <antonino.rando.2003@gmail.com>
2022-04-24 01:07:53 +0200

    Added 'rappresentazione' to 'Costante'

--- Costante.java
+++ Costante.java
@@ -3,5 +3,6 @@ package EspressioniDiVerita;
 public class Costante extends Espressione {
     public Costante(ValoreVerita stato) {
         this.stato = stato;
+        rappresentazione = stato.toString();
     }
 }
